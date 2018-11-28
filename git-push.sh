8624542a163dbb97a967ec09c55e96b2d70dbbbe
prakharbhatt007 <prkhrbhtt@gmail.com>
2018-11-28 04:43:42 -0800

    $commit_msg

--- git-push.sh
+++ git-push.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 git status
-echo "adding all files to git"
-git add .
-
-echo "Enter the commit message"
-read commit_msg
-git commit -m $"commit_msg"
+echo "Command: 'all' for adding all files 'cm': for adding files one by one "
+read cmd
+if [ $cmd = "all" ];then
+	echo "adding all files to git"
+	git add .
+elif [ $cmd ="cm" ];then
+	while [ $cmd !="end"];do
+		echo "give /"end/" to stop adding files"
+		echo "Add file name"
+		read file
+		git add $file
+		echo "Enter the commit message"
+		read commit_msg
+		git commit -m $"commit_msg"
+		echo "give end or con"
+		read cmd
+	done
+fi
 
 echo "Enter the branch name"
 read branch_name
