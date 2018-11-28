f729e8366b6989d40a27f617688f6a82933f0170
prakharbhatt007 <prkhrbhtt@gmail.com>
2018-11-28 03:59:02 -0800

    fake commit

--- git-push.sh
+++ git-push.sh
@@ -1,8 +1,15 @@
-echo "adding files to git"
-git add .
+git status
+echo "Enter the name of file you want to add"
+read files
+if [ "$files" = "all" ];then
+	echo "adding all files to git"
+	git add .
+fi
+git add $files
 echo "Enter the commit message"
 read commit_msg
 git commit -m $commit_msg
+
 echo "Enter the branch name"
-read branch_name
+#read branch_name
 git push origin $branch_name
