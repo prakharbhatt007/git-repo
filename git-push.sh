a8b5faf0d7ad339858ebc9f49142b59e9984c0de
prakharbhatt007 <prkhrbhtt@gmail.com>
2018-11-28 03:40:24 -0800

    checking

---
+++ git-push.sh
@@ -0,0 +1,8 @@
+echo "adding files to git"
+git add .
+echo "Enter the commit message"
+read commit_msg
+git commit -m $commit_msg
+echo "Enter the branch name"
+read branch_name
+git "push origin $branch_name"
